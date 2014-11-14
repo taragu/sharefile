f0368b0120426df7ccee9c57b6c645689a7cbd96
Yu Fan <Adeluxe@users.noreply.github.com>
2014-11-14 00:33:27 -0500

    modification

--- sharefile/Client/main.cpp
+++ sharefile/Client/main.cpp
@@ -4,7 +4,7 @@ int main( int argc, char **argv )
 {
 	if ( argc < 3 )
 	{
-		std::cout << argv[0] << "<IP><端口>" << std::endl ;
+		std::cout << argv[0] << "<IP><Port>" << std::endl ;
 		return - 1 ;
 	}
 	ClientCommand clientcommand( argv[1], atoi(argv[2]) )  ;
