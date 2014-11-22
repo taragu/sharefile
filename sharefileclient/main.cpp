dbb67829b919c50b5b445a65020e14822e31da24
taragu <tara.weiqing@gmail.com>
2014-11-21 18:46:08 -0800

    added sharefileclient

---
+++ sharefileclient/main.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "clientcommand.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+
+
+    if ( argc < 3 )
+        {
+            std::cout << argv[0] << "<IP><Port>" << std::endl ;
+            return - 1 ;
+        }
+        ClientCommand clientcommand( argv[1], atoi(argv[2]) )  ;
+        clientcommand.manager() ;
+
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+
+    return a.exec();
+}
---
+++ sharefileclient/sharefileclient.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-11-21T17:13:42
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = sharefileclient
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    clientcommand.cpp
+
+HEADERS  += mainwindow.h \
+    clientcommand.h
+
+FORMS    += mainwindow.ui
